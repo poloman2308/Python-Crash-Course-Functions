ecb1ff5cf5501eac236843bf81c9e28c7fb21dd1
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-23 22:41:39 -0500

    Add files via upload

---
+++ favorite_book.py
@@ -0,0 +1,10 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+def favorite_book(title):
+	"""Message displaying my favorite book."""
+	print(title + " is one of my favorite books.")
+
+favorite_book('Alice in Wonderland')
+
