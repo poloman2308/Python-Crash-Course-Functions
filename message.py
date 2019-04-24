71ed9eda2fa12afe2a2e3321e0f754b1da99fc32
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-23 22:35:03 -0500

    Add files via upload

---
+++ message.py
@@ -0,0 +1,11 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+def display_message():
+	"""Display message of what I am learning."""
+	msg = "I'm learning to store code in functions."
+	print(msg)
+
+display_message()
+
