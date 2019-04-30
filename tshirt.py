5d885905b16099f6a892d72598e85cf3166133cf
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-29 23:09:56 -0500

    Add files via upload

---
+++ tshirt.py
@@ -0,0 +1,12 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+def make_shirt(size, message):
+	"""Summarize the shirt that's going to be made."""
+	print("\nI'm going to make a " + size + " t-shirt.")
+	print('It will say "' + message + '"')
+
+make_shirt('large', 'I love Python!')
+make_shirt(message="Readability counts.", size='medium')
+
