1ae854c7e67dcdde6d490991c6c430c1e4c6cc9c
Derek Acevedo <dacevedo86@yahoo.com>
2019-04-29 23:10:07 -0500

    Add files via upload

---
+++ large_shirt.py
@@ -0,0 +1,13 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+def make_shirt(size='large', message='I love Python!'):
+	"""Summarize the shirt that's going to be made."""
+	print("\nI'm going to make a " + size + " t-shirt.")
+	print('It will say "' + message + '"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'Programmers are loopy.')
+
